989b464e3ec4deee9ed59e81e2efb9d9a8f5a744
Olivier Francon <olivier.francon@cognizant.com>
2019-12-24 01:22:39 +0100

    Upgrade version to 1.0.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def _read(fname):
 
 setup(
     name='rio-sdk',
-    version='1.0.2',
+    version='1.0.3',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     packages=['rio_sdk', 'rio_sdk.generated', 'rio_sdk.protos'],
     package_data={
